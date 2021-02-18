b9942e6a28d2ad444dd6ffd89a379b142c830bec
Boris Stanojevic <borisstanojevicfkkt@gmail.com>
2021-02-18 01:14:51 +0100

    Create use-page-visibility.js

---
+++ use-page-visibility.js
@@ -0,0 +1,33 @@
+import {useRef, useEffect} from "react";
+
+const VISIBILITY_CHANGE = "visibilitychange";
+
+const subscribeToVisibilityChange = (callback) =>
+  document.addEventListener(VISIBILITY_CHANGE, callback);
+
+const unsubscribeFromVisibilityChange = (callback) =>
+  document.removeEventListener(callback, VISIBILITY_CHANGE);
+
+const isPageHidden = () => document.visibilityState === "hidden";
+
+const isPageVisible = () => document.visibilityState === "visible";
+
+function usePageVisiblity(onHidden, onVisible) {
+  const handleVisibilityChange = useRef(null);
+
+  useEffect(() => {
+    if (!handleVisibilityChange.current) {
+      handleVisibilityChange.current = function () {
+        if (isPageHidden()) onHidden && onHidden();
+
+        if (isPageVisible()) onVisible && onVisible();
+      };
+    }
+
+    subscribeToVisibilityChange(handleVisibilityChange.current);
+
+    return () => {
+      unsubscribeFromVisibilityChange(handleVisibilityChange.current);
+    };
+  }, []);
+}
